5ada71cda559db9a1ae85a352e47986ba45d08c5
Giofist <Giofist@users.noreply.github.com>
2020-05-16 16:44:58 +0200

    Major restructuuring of the header
    Now the header is adaptive to the screen size

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -43,3 +43,34 @@ carouselSlide.addEventListener('transitionend',() => {
 function popUp() {
   alert("Due to the currrent COVID-19 situation all the association activities are suspended!");
 }
+
+
+
+
+const navSlide = () =>{
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+ 
+  burger.addEventListener('click', ()=>{
+    //toggle nav
+    nav.classList.toggle('nav-active');
+    
+    //animate links
+    navLinks.forEach((link, index) =>{
+      if(link.style.animation){
+        link.style.animation = '';
+      }
+      else{
+        link.style.animation = 'navLinkFade 0.5s ease forwards 0.2s';
+        console.log(index);
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle('toggle');
+  });
+
+ 
+}
+
+navSlide();
