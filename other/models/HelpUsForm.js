09facba046c4e23a2e18ae85b8c83278046c2137
davidesiciliano <davidegiuseppe.siciliano@mail.polimi.it>
2020-05-22 21:53:24 +0200

    [HelpUsForm] post to database

---
+++ other/models/HelpUsForm.js
@@ -0,0 +1,15 @@
+const table = {
+  getTable: "helpUsForm",
+  id: "id",
+  name: "name",
+  surname: "surname",
+  birthDate: "birthDate",
+  address: "address",
+  city: "city",
+  province: "province",
+  phoneNumber: "phoneNumber",
+  email: "email",
+  skills: "skills"
+}
+
+module.exports = table;
---
+++ other/service/HelpUsFormService.js
@@ -0,0 +1,55 @@
+'use strict';
+
+let HelpUs = require("../models/HelpUsForm")
+
+let sqlDb;
+
+exports.helpUsFormDbSetup = function (connection) {
+  sqlDb = connection;
+  console.log("Checking if the helpUsForm table exists");
+  return sqlDb.schema.hasTable(HelpUs.getTable)
+    .then((exists) => {
+      if (!exists) {
+        console.log("It does not exist so create it");
+        return sqlDb.schema.createTable(HelpUs.getTable, tableBuilder => {
+          tableBuilder.increments();
+          tableBuilder.integer(HelpUs.id);
+          tableBuilder.text(HelpUs.name);
+          tableBuilder.text(HelpUs.surname);
+          tableBuilder.text(HelpUs.birthDate);
+          tableBuilder.text(HelpUs.address)
+          tableBuilder.text(HelpUs.city);
+          tableBuilder.text(HelpUs.province);
+          tableBuilder.text(HelpUs.phoneNumber);
+          tableBuilder.text(HelpUs.email);
+          tableBuilder.text(HelpUs.skills);
+        });
+      } else {
+        console.log("Table already exists");
+      }
+    }).catch(error => console.log(error));
+};
+
+/**
+ * HelpUs form
+ * Send a form to be recontacted
+ *
+ * helpUsform HelpUsForm the form to insert (optional)
+ * no response value expected for this operation
+ **/
+exports.helpUsFormPOST = function(helpUsForm) {
+  return sqlDb(HelpUs.getTable)
+    .insert({
+      id: helpUsForm.id,
+      name: helpUsForm.name,
+      surname: helpUsForm.surname,
+      birthDate: helpUsForm.birthDate,
+      address: helpUsForm.address,
+      city: helpUsForm.city,
+      province: helpUsForm.province,
+      phoneNumber: helpUsForm.phoneNumber,
+      email: helpUsForm.email,
+      skills: helpUsForm.skills
+    });
+}
+
