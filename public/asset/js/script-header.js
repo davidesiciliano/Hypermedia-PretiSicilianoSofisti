c44760694d6d334e2545506421791e26db9a8768
davidesiciliano <davidegiuseppe.siciliano@mail.polimi.it>
2020-05-23 12:13:52 +0200

    [Header] fix selected

--- public/asset/js/script-header.js
+++ public/asset/js/script-header.js
@@ -1,6 +1,6 @@
 function loadHeader() {
   var header = document.querySelector(".header");
-  const currentPage = header.id.replace("Page", "");
+  const currentPage = header.id.replace("Header", "");
   header.innerHTML = `
     <div class="logo">
       <a href="../../index.html">
@@ -15,8 +15,8 @@ function loadHeader() {
       <li id="events"><a href="../../pages/events_page.html">Events</a></li>
       <li id="people"><a href="../../pages/people_page.html">People</a></li>
       <li id="helpUs"><a href="../../pages/helpUs_page.html">Help Us</a></li>
-      <li class="FAQLink selected"><a href="../../pages/FAQ_page.html">FAQ</a></li>
-      <li class="TermsLink"><a href="../../pages/terms_page.html">Terms</a></li>
+      <li id="faq" class="FAQLink"><a href="../../pages/FAQ_page.html">FAQ</a></li>
+      <li id="terms" class="TermsLink"><a href="../../pages/terms_page.html">Terms</a></li>
     </ul>
     <div class="burger">
       <div class="line1"></div>
@@ -25,7 +25,7 @@ function loadHeader() {
     </div>
   `;
 
-  document.getElementById(currentPage).className = "selected";
+  document.getElementById(currentPage).classList.add("selected");
 }
 
 loadHeader();
